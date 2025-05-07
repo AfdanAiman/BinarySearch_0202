ea1ae058e5d75801efa7333aac514ac965c3f295
AfdanAiman <afdanaiman06@gmail.com>
2025-05-07 15:04:05 +0700

    menambahkan fungsi display

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -47,6 +47,22 @@ void bubleSortArray()
         }
         pass++;
     }    while (pass <= nPanjang - 1);
-    
+
+}
+
+void display()
+{
+    cout << "\n================================\n";
+    cout << "    Elemen Array Setelah Diurutkan  (Asc)  \n";
+    cout << "==================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " ->> ";
+        }
+    }
+    cout << endl;
 }
     
